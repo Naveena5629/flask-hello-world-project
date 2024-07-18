b29d87192612b22ef99a0ad4a03f3bf3e9f0d6e7
Naveena5629 <naveenabalasundari@gmail.com>
2024-07-18 15:59:45 +0530

    Add files via upload

--- hello.py
+++ hello.py
@@ -3,6 +3,6 @@ from flask import Flask
 app=Flask(__name__)
 @app.route('/')
 def hello_world():
-    return "<div><cente><h1>Hello World!</center></h1></div>"
+    return "<div style='background:skyblue'><cente><h1>Hello World!</center></h1></div>"
 if __name__=='__main__':
     app.run(debug=True)
